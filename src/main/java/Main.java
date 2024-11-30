790226622604f74b7633f5e1715363ba612ff31c
Daniil Kopanev <DKopanev@inno.tech>
2024-11-30 12:45:16 +0300

    Изменена логика работы Main.java, добавлено выполнение арифметических операций на введенными числами.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+       System.out.println("Введите первое число:");
+       int firstNumber = new Scanner(System.in).nextInt();
+       System.out.println("Введите второе число:");
+       int secondNumber = new Scanner(System.in).nextInt();
+       int sum = firstNumber + secondNumber;
+       int subt = firstNumber - secondNumber;
+       int multipl = firstNumber * secondNumber;
+       double quotient = (double)firstNumber / secondNumber;
+       System.out.println("Операции над введенными числами:");
+       System.out.println("Сумма: " + sum);
+       System.out.println("Разность: " + subt);
+       System.out.println("Произведение: " + multipl);
+       System.out.println("Частное: " + quotient);
     }
 }
