e82a94240246655d802e52380c57a16166ab9705
Daniil Kopanev <dkopanev@inno.tech>
2025-01-26 21:27:34 +0300

    1. Изменена логика работы Main.java: добавлена логика парсинга строки файла, также реализована логика подчета запросов от Googlebot и YandexBot 2. Удалена логика вычисления самой длинной и короткой строк.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int count = 1;
         int countLine = 0;
-        int minLength = 0;
-        int maxLength = 0;
+        int countGoogle = 0;
+        int countYandex = 0;
+
+        String ipAddress, date, pathRequest, requestCode, sizeData, pathUrl, userAgent;
 
         while (true) {
             System.out.println("Введите путь до файла:");
@@ -28,31 +32,51 @@ public class Main {
                     if (length > 1024) {
                         throw new MaxLengthLineException("Maximum line length exceeded 1024");
                     }
-                    if (minLength == 0) {
-                        minLength = length;
-                    }
-                    if (minLength > length) {
-                        minLength = length;
-                    }
-                    if (maxLength < length) {
-                        maxLength = length;
-                    }
                     countLine++;
+                    List<String> l = new ArrayList<String>();
+                    l = List.of(line.split(" "));
+                    ipAddress = l.get(0);
+                    date = l.get(3) + l.get(4);
+                    pathRequest = l.get(5) + l.get(6);
+                    requestCode = l.get(8);
+                    sizeData = l.get(9);
+                    pathUrl = l.get(10);
+                    userAgent = "";
+                    if (!l.get(11).equals("\"-\"")) {
+                        for (int i = 0; i < l.size() - 11; i++) {
+                            userAgent = userAgent + l.get(11 + i);
+                        }
+                    } else userAgent = l.get(11);
+                    try {
+                        if (!(userAgent.equals("\"-\""))) {
+                            userAgent = userAgent.substring(userAgent.indexOf('('));
+                            String[] parts = userAgent.split(";");
+                            if (parts.length >= 2) {
+                                String fragment = parts[1];
+                                fragment = fragment.trim().substring(0, fragment.indexOf('/'));
+                                if (fragment.equals("Googlebot")) {
+                                    countGoogle++;
+                                }
+                                if (fragment.equals("YandexBot")) {
+                                    countYandex++;
+                                }
+                            }
+                        }
+                    } catch (IndexOutOfBoundsException ignored) {}
                 }
             } catch (MaxLengthLineException ex) {
                 System.out.println(ex);
                 System.exit(0);
-            } catch (Exception  ex) {
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + count);
             count++;
             System.out.println("Общее количество строк в файле: " + countLine);
-            System.out.println("Длина самой длинной строки в файле: " + maxLength);
-            System.out.println("Длина самой короткой строки в файле: " + minLength);
+            System.out.println("Доля запросов от Googlebot: " + (double) countGoogle / countLine);
+            System.out.println("Доля запросов от YandexBot : " + (double) countYandex / countLine);
             System.out.println("-----------------------------------------------------------");
         }
-
     }
 }
